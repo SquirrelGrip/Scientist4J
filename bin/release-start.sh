9c75ab4a5458a445a6f33a8134f2f5bbe52b8f15
Adrian <adrian.richter@gmail.com>
2020-03-19 17:07:25 +0800

    [gitflow] updating poms for 0.2-SNAPSHOT development

--- bin/release-start.sh
+++ bin/release-start.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-git fetch "origin"
+CURRENT_RELEASE_VERSION=$1
+NEXT_DEVELOP_SNAPSHOT=$2
+
+git fetch origin
 git checkout -B develop remotes/origin/develop --
 
 # create release branch
-git branch release/${CURRENT_RELEASE_VERSION}
+git branch "release/${CURRENT_RELEASE_VERSION}"
 
 # update the versions on develop to the next -SNAPSHOT version
-call mvn versions:set -DgenerateBackupPoms=false -DnewVersion=${NEXT_DEVELOP_SNAPSHOT}
+call mvn versions:set -DgenerateBackupPoms=false "-DnewVersion=${NEXT_DEVELOP_SNAPSHOT}"
 git commit -a -m "[gitflow] updating poms for ${NEXT_DEVELOP_SNAPSHOT} development"
 
 # push the changes atomically
-git push --atomic origin develop release/${CURRENT_RELEASE_VERSION}
+git push --atomic origin develop "release/${CURRENT_RELEASE_VERSION}"
