4653a00db960c0a640a756d96dc0469648d323bc
Adrian <adrian.richter@gmail.com>
2020-04-13 14:07:24 +0800

    Added windows certificate keystore

--- scientist4k-http/src/main/kotlin/com/github/squirrelgrip/scientist4k/SecuredServer.kt
+++ scientist4k-http/src/main/kotlin/com/github/squirrelgrip/scientist4k/SecuredServer.kt
@@ -15,9 +15,6 @@ open class SecuredServer(
 
     init {
         val serverConnectors = serverConfiguration.getServerConnectors(server)
-//        serverConnectors.forEach {
-//            println(it.port)
-//        }
         server.connectors = serverConnectors
         server.sessionIdManager = DefaultSessionIdManager(server)
         ContextHandler("/").also { contextHandler ->
