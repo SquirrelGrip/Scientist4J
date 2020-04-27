fdbcf06aa0f3ee2b89c758c2b644790128dd0eb4
Adrian <adrian.richter@gmail.com>
2020-04-27 17:53:22 +0800

    Replacing JSON to Map with JsonDiff

--- scientist4k-http-core/src/main/kotlin/com/github/squirrelgrip/scientist4k/comparator/JsonContentTypeComparator.kt
+++ scientist4k-http-core/src/main/kotlin/com/github/squirrelgrip/scientist4k/comparator/JsonContentTypeComparator.kt
@@ -1,30 +1,30 @@
 package com.github.squirrelgrip.scientist4k.comparator
 
-import com.github.squirrelgrip.extension.json.toInstance
-import com.github.squirrelgrip.extension.map.flatten
+import com.fasterxml.jackson.databind.JsonNode
+import com.github.fge.jsonpatch.diff.JsonDiff
+import com.github.squirrelgrip.extension.json.toJson
+import com.github.squirrelgrip.extension.json.toJsonNode
 import com.github.squirrelgrip.scientist4k.model.ComparisonResult
 import com.github.squirrelgrip.scientist4k.model.ComparisonResult.Companion.SUCCESS
-import com.google.common.collect.MapDifference
-import com.google.common.collect.Maps
+
 
 class JsonContentTypeComparator : ContentTypeComparator {
 
     override fun invoke(control: ByteArray, candidate: ByteArray): ComparisonResult {
-        val controlMap: Map<String, Any?> = control.toInstance()
-        val candidateMap: Map<String, Any> = candidate.toInstance()
+        val controlJsonNode = control.toJsonNode()
+        val candidateJsonNode = candidate.toJsonNode()
 
-        val difference: MapDifference<String, Any> = Maps.difference(controlMap.flatten(), candidateMap.flatten())
+        val diff: JsonNode = JsonDiff.asJson(controlJsonNode, candidateJsonNode)
+        println(diff.toJson())
 
-        return if (difference.areEqual()) {
+        val list = diff.asIterable().toList()
+        return if (list.isEmpty()) {
             SUCCESS
         } else {
             ComparisonResult(
-                    *difference.entriesOnlyOnLeft()
-                            .map { (key, _) -> "$key in control, not in candidate" }.toTypedArray(),
-                    *difference.entriesOnlyOnRight()
-                            .map { (key, _) -> "$key in candidate, not in control" }.toTypedArray(),
-                    *difference.entriesDiffering()
-                            .map { (key, value) -> "$key has different values (${value.leftValue()} != ${value.rightValue()})" }.toTypedArray()
+                    *list.map {
+                        toJson()
+                    }.toTypedArray()
             )
         }
     }
