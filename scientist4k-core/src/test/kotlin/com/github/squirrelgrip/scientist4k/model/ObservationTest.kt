0089c4c00f28e6e5367380bb92d0fb8282e38b79
Adrian <adrian.richter@gmail.com>
2020-05-01 22:06:46 +0800

    A lot of change

--- scientist4k-core/src/test/kotlin/com/github/squirrelgrip/scientist4k/model/ObservationTest.kt
+++ scientist4k-core/src/test/kotlin/com/github/squirrelgrip/scientist4k/model/ObservationTest.kt
@@ -6,6 +6,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class ObservationTest {
+
     @Test
     fun `Observation set value to null`() {
         val observation = Observation<Int>("observation", NoopMetricsProvider().timer())
