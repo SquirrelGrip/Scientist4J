a564b5daa8f1c87a99d02f38129972e84b1cd4e6
Adrian <adrian.richter@gmail.com>
2020-05-12 00:06:59 +0800

    Fixed breaking tests

--- scientist4k-report-api/src/main/kotlin/com/github/squirrelgrip/report/repository/FileExperimentRepository.kt
+++ scientist4k-report-api/src/main/kotlin/com/github/squirrelgrip/report/repository/FileExperimentRepository.kt
@@ -3,8 +3,7 @@ package com.github.squirrelgrip.report.repository
 import com.github.squirrelgrip.extension.json.toInstance
 import com.github.squirrelgrip.report.exception.ExperimentNotFoundException
 import com.github.squirrelgrip.report.model.ExperimentReport
-import com.github.squirrelgrip.scientist4k.core.model.ExperimentResult
-import com.github.squirrelgrip.scientist4k.http.core.model.ExperimentResponse
+import com.github.squirrelgrip.scientist4k.http.core.model.HttpExperimentResult
 import org.springframework.stereotype.Repository
 import java.io.File
 
@@ -12,17 +11,19 @@ import java.io.File
 class FileExperimentRepository(
         val baseDirectory: File
 ) : ExperimentRepository {
-    override fun findAllExperiments(): List<String> {
-        return baseDirectory.list { dir, _ ->
-            dir.isDirectory()
+   @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+   override fun findAllExperiments(): List<String> {
+        return baseDirectory.list { file, _ ->
+            file.isDirectory()
         }.toList()
     }
 
+    @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
     override fun findExperimentByName(name: String): ExperimentReport {
         val experimentDirectory = File(baseDirectory, name)
         if (experimentDirectory.exists()) {
             val list = experimentDirectory.listFiles().map {
-                it.toInstance<ExperimentResult<ExperimentResponse>>()
+                it.toInstance<HttpExperimentResult>()
             }
             return ExperimentReport(name, list)
         }
